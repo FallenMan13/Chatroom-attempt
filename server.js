6ea04d45ab31bbe2a7f2176245e6f9e468c06968
FallenMan13 <rosscraig9@gmail.com>
2017-03-28 16:38:39 +0100

    Redis storage added

--- server.js
+++ server.js
@@ -2,6 +2,17 @@ var express = require("express");
 var bodyParser = require("body-parser");
 var session = require("express-session");
 var app = express();
+var redis = require("redis");
+var client = redis.createClient();
+
+client.on("error", function (err){
+  console.log("Error " + err);
+  client.quit();
+});
+
+client.on('connect', function() {
+    console.log('Redis client connected');
+});
 
 app.set("port", (process.env.PORT || 5000));
 
@@ -16,7 +27,15 @@ app.set("views", __dirname + "/views");
 app.set("view engine", "ejs");
 
 app.get("/", function(request, response){
-  response.render("pages/entry");
+  client.lrange("message", 0, -1, function(error, reply){
+    if(reply === null){
+      response.render("pages/entry");
+    }
+    else if(reply !== null){
+      client.del("message");
+      response.render("pages/entry");
+    }
+  });
 });
 
 app.post("/", function(request, response){
@@ -29,13 +48,33 @@ app.get("/chatroom", function(request, response){
     response.redirect("/"); // Send the user back to the name entry page
   }
   else{
-    var message = Object.assign({message: "Welcome to the chatroom, type something below to get started!"}, request.session.name);
-    response.render("pages/chatroom", message);
+    client.lrange("message", 0, -1, function(error, reply){
+      if(reply.length === 0){
+        var message = Object.assign({message: "Welcome to the chatroom, type something below to get started!"}, request.session.name);
+        response.render("pages/chatroom", message);
+      }
+      else if(reply.length !== 0){
+        var message = Object.assign({message: reply.toString().replace(/,/g, "\n")}, request.session.name);
+        response.render("pages/chatroom", message);
+      }
+    });
   }
 });
 
 app.post("/chatroom", function(request, response){
   // Code to update textarea with sent message
+  client.lrange("message", 0, -1, function(error, reply){
+    if(reply.length === 0){
+      client.rpush(["message", request.session.name.name + ": " + request.body.message], function(){
+        response.redirect("/chatroom");
+      })
+    }
+    else if(reply.length !== 0){
+      client.rpush(["message", request.session.name.name + ": " + request.body.message], function(){
+        response.redirect("/chatroom");
+      })
+    }
+  })
 })
 
 app.listen(app.get("port"), function(){
