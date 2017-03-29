704cb50e30a80c0b6cbd4d6c0445d0c81728193d
FallenMan13 <rosscraig9@gmail.com>
2017-03-29 09:31:16 +0100

    Redis storage added

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ client.on("error", function (err){
 });
 
 client.on('connect', function() {
-    console.log('Redis client connected');
+  console.log('Redis client connected');
 });
 
 app.set("port", (process.env.PORT || 5000));
@@ -54,7 +54,7 @@ app.get("/chatroom", function(request, response){
         response.render("pages/chatroom", message);
       }
       else if(reply.length !== 0){
-        var message = Object.assign({message: reply.toString().replace(/,/g, "\n")}, request.session.name);
+        var message = Object.assign({message: reply.toString().replace(/,&R!\//g, "\n")}, request.session.name);
         response.render("pages/chatroom", message);
       }
     });
@@ -64,13 +64,13 @@ app.get("/chatroom", function(request, response){
 app.post("/chatroom", function(request, response){
   // Code to update textarea with sent message
   client.lrange("message", 0, -1, function(error, reply){
-    if(reply.length === 0){
+    if(reply.length === 0){ // If no messages have been sent yet
       client.rpush(["message", request.session.name.name + ": " + request.body.message], function(){
         response.redirect("/chatroom");
       })
     }
-    else if(reply.length !== 0){
-      client.rpush(["message", request.session.name.name + ": " + request.body.message], function(){
+    else if(reply.length !== 0){ // If there are messages stored in the Redis database
+      client.rpush(["message", "&R!/" + request.session.name.name + ": " + request.body.message], function(){ // "&R!/" will be replaced with a newline character when the message is displayed as a "," will be placed before it, allowing the regex to replace it with "\n"
         response.redirect("/chatroom");
       })
     }
