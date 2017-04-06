73cd5aecf263bd536ba19eb4d5c9dc33073b09e0
FallenMan13 <rosscraig9@gmail.com>
2017-04-06 13:32:54 +0100

    Auth0 connection implemented

---
+++ passport.js
@@ -0,0 +1,38 @@
+module.exports = function (app){
+  var passport = require('passport');
+  var Auth0Strategy = require('passport-auth0');
+
+  var strategy = new Auth0Strategy({
+      domain:       "fallenman13.eu.auth0.com",
+      clientID:     "ddCpIAzWF0jU4N4XDWggADYI1ASUdIml",
+      clientSecret: "PsSWY8e7NCoFKtwD8fjRGKFEsfI_PVuGKV63KLE3RtOHecjM1kA2JZC4TOnSNAeX",
+      callbackURL:  "http://" + app.get("ipaddr") + ":" + app.get("port") + "/callback"
+    }, function(accessToken, refreshToken, extraParams, profile, done) {
+      // accessToken is the token to call Auth0 API (not needed in the most cases)
+      // extraParams.id_token has the JSON Web Token
+      // profile has all the information from the user
+      return done(null, profile);
+    });
+
+  passport.use(strategy);
+
+  // This can be used to keep a smaller payload
+  passport.serializeUser(function(user, done) {
+    done(null, user);
+  });
+
+  passport.deserializeUser(function(user, done) {
+    done(null, user);
+  });
+
+  app.use(passport.initialize());
+  app.use(passport.session());
+
+  app.get('/callback',
+  passport.authenticate('auth0', { failureRedirect: '/url-if-something-fails' }),
+  function(req, res) {
+    var users = app.get("users")
+    users.push(req.user)
+    res.redirect(req.session.returnTo || '/chatroom');
+  });
+}
--- server.js
+++ server.js
@@ -1,13 +1,13 @@
 var express = require("express");
 var bodyParser = require("body-parser");
 var session = require("express-session");
+var ensureLoggedIn = require('connect-ensure-login').ensureLoggedIn("/");
 var app = express();
 var redis = require("redis");
 var client = redis.createClient();
 var http = require("http").createServer(app);
 var io = require("socket.io").listen(http);
-var users = [];
-var i = 1;
+var pass = require("./passport")
 
 client.on("error", function (err){
   console.log("Error " + err);
@@ -20,11 +20,13 @@ client.on('connect', function() {
 
 app.set("ipaddr", "10.78.31.53");
 app.set("port", (process.env.PORT || 80));
-
+app.set("users", []);
 app.use(session({
   secret: "it's a secret to everybody",
   saveUninitialized: true
 }));
+pass(app);
+
 app.use(express.static(__dirname + "/public"));
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(bodyParser.json());
@@ -32,56 +34,41 @@ app.set("views", __dirname + "/views");
 app.set("view engine", "ejs");
 
 app.get("/", function(request, response){
-  if(request.session.name !== undefined){ // If a user has signed out, destroy the session to prevent them from entering the chatroom by adding "/chatroom" to the end of the url
-    users.splice(users.indexOf(request.session.name), 1);
-    io.sockets.emit("connectedChange");
-    request.session.destroy();
-  }
-    response.render("pages/entry");
+  response.render("pages/entry", {redirURL: "http://" + app.get("ipaddr") + ":" + app.get("port") + "/callback"});
 });
 
 app.post("/", function(request, response){
-  if(users.includes(request.body.name)){
-    request.session.name = request.body.name + "(" + i++ + ")";
-    users.push(request.session.name);
-  }
-  else{
-    request.session.name = request.body.name;
-    users.push(request.body.name);
-  }
-  io.sockets.emit("connectedChange");
   response.redirect("/chatroom");
 });
 
-app.get("/chatroom", function(request, response){
-  if(request.session.name === undefined){ // Attempt to prevent a crash from a server restart causing the username to be lost
-    response.redirect("/"); // Send the user back to the name entry page
-  }
-  else{
-    client.lrange("message", 0, -1, function(error, reply){
-      if(request.headers.accept === "application/json"){
-        response.send({messages: reply, users: users});
-      }
-      else{
-        response.render("pages/chatroom", {messages: reply, name: request.session.name, users: users});
-      }
-    });
-  }
+app.get("/chatroom", ensureLoggedIn, function(request, response){
+  io.sockets.emit("connectedChange");
+  var users = app.get("users")
+  client.lrange("message", 0, -1, function(error, reply){
+    if(request.headers.accept === "application/json"){
+      response.send({messages: reply, users: users});
+    }
+    else{
+      response.render("pages/chatroom", {messages: reply, name: request.user.nickname, users: users});
+    }
+  });
 });
 
-app.post("/chatroom", function(request, response){
-  if(request.session.name === undefined){
-    io.sockets.emit("noUsername");
-    response.redirect("/");
-  }
-  else{
-    client.lrange("message", 0, -1, function(error, reply){
-      client.rpush(["message", request.session.name + ": " + request.body.message], function(){
-        io.sockets.emit("newMessage");
-        response.redirect("/chatroom");
-      })
+app.post("/chatroom", ensureLoggedIn, function(request, response){
+  client.lrange("message", 0, -1, function(error, reply){
+    client.rpush(["message", request.user.nickname + ": " + request.body.message], function(){
+      io.sockets.emit("newMessage");
+      response.redirect("/chatroom");
     })
-  }
+  })
+})
+
+app.get("/logout", function(request, response){
+  var users = app.get("users");
+  users.splice(users[users.indexOf(request.user.nickname)], 1)
+  io.sockets.emit("connectedChange");
+  request.logout();
+  response.redirect("/");
 })
 
 http.listen(app.get("port"), app.get("ipaddr"), function(){
